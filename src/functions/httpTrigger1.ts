3d987736bdcc75ffc8022bc01d70fa04c0525bac
raioramalho <ramalho.sit@gmail.com>
2024-02-22 21:23:55 -0300

    first-azure-function

---
+++ src/functions/httpTrigger1.ts
@@ -0,0 +1,16 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function httpTrigger1(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`Http function processed request for url "${request.url}"`);
+    const produtos = await prisma.produto.findMany();
+    return { body: JSON.stringify(produtos) };
+};
+
+app.http('httpTrigger1', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: httpTrigger1
+});
